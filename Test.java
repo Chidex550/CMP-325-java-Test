89ebe85c4ce5dcd6e219f02755357bcbbc5e4f29
Chidex550 <168143091+Chidex550@users.noreply.github.com>
2024-11-14 22:37:50 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,39 @@
+package Test;
+
+public class Test {
+
+    @Test
+    public void testIsPrime() {
+        assertTrue(isPrime(7));
+        assertTrue(isPrime(4));
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(isPalindrome("level"));
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
